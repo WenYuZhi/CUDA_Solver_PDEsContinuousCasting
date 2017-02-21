3c7dfdb8f9692665bd161566b51849d66b910fd5
wangyuan <yuanwangshui@126.com>
2017-02-21 08:54:14 +0800

    add T_Middle
    add T_Middle

--- First_cuda_gp/kernel.cu
+++ First_cuda_gp/kernel.cu
@@ -14,7 +14,7 @@ cudaError_t addWithCuda(float *T_Init, float dx, float dy, float tao, int nx, in
 __device__ void Physicial_Parameters(float T, float *pho, float *Ce, float *lamd);
 __device__ float Boundary_Condition(int j, int ny, float dx, float *ccml_zone, float *H_Init);
 
-__global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Init, float dx, float dy, float tao, int nx, int ny, bool disout)
+__global__ void MainKernel(float *T_New, float *T_Last, float *ccml, float *H_Init, float dx, float dy, float tao, int nx, int ny, bool disout)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = threadIdx.y;
@@ -22,28 +22,31 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 	int D = ny;
 
 	float pho, Ce, lamd; // physical parameters pho represents desity, Ce is specific heat and lamd is thermal conductivity
-	float a, T_Up, T_Down, T_Right, T_Left, h = 200.0, Tw = 30.0, Vcast = -0.02, T_Cast = 1558.0;
+	float a, T_Up, T_Down, T_Right, T_Left, T_Middle, h = 0.0, Tw = 30.0, Vcast = -0.02, T_Cast = 1558.0;
+
+	Physicial_Parameters(T_Last[idx], &pho, &Ce, &lamd);
+	a = (lamd) / (pho*Ce);
+	h = Boundary_Condition(i, ny, dy, ccml, H_Init);
 
 	if (disout) {
-		Physicial_Parameters(T_Last[idx], &pho, &Ce, &lamd);
-		a = (lamd) / (pho*Ce);
-		h = Boundary_Condition(i, ny, dy, ccml, H_Init);
 		if (j == 0 && i != 0 && i != ny - 1) //1
 		{
+			T_Middle = T_Last[idx];
 			T_Up = T_Last[idx + D];
-			T_Down = T_Last[idx + D] - 2 * dx * h * (T_Last[idx] - Tw) / lamd;
+			T_Down = T_Last[idx + D] - 2 * dx * h * (T_Middle - Tw) / lamd;
 			T_Right = T_Last[idx + 1];
 			T_Left = T_Last[idx - 1];
-			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Last[idx] +
+			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (j == nx - 1 && i != 0 && i != ny - 1)  //2
 		{
+			T_Middle = T_Last[idx];
 			T_Up = T_Last[idx - D];
 			T_Down = T_Last[idx - D];
 			T_Right = T_Last[idx + 1];
 			T_Left = T_Last[idx - 1];
-			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Last[idx] +
+			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (i == 0 && j != 0 && j != nx - 1)  //3
@@ -52,11 +55,12 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 		}
 		else if (i == ny - 1 && j != 0 && j != nx - 1)  //4
 		{
+			T_Middle = T_Last[idx];
 			T_Up = T_Last[idx + D];
 			T_Down = T_Last[idx - D];
 			T_Right = T_Last[idx - 1];
 			T_Left = T_Last[idx - 1];
-			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Last[idx] +
+			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (j == 0 && i == 0)  //5
@@ -65,11 +69,12 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 		}
 		else if (j == 0 && i == ny - 1)  //6
 		{
+			T_Middle = T_Last[idx];
 			T_Up = T_Last[idx + D];
 			T_Down = T_Last[idx + D];
 			T_Right = T_Last[idx - 1];
 			T_Left = T_Last[idx - 1];
-			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Last[idx] +
+			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (j == nx - 1 && i == 0)   //7
@@ -78,45 +83,46 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 		}
 		else if (j == nx - 1 && i == ny - 1) //8
 		{
+			T_Middle = T_Last[idx];
 			T_Up = T_Last[idx - D];
 			T_Down = T_Last[idx - D];
 			T_Right = T_Last[idx - 1];
 			T_Left = T_Last[idx - 1];
-			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Last[idx] +
+			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
-		else
+		else if (j != 0 && j != nx - 1 && i != 0 && i != ny - 1) //9
 		{
+			T_Middle = T_Last[idx];
 			T_Up = T_Last[idx + D];
 			T_Down = T_Last[idx - D];
 			T_Right = T_Last[idx + 1];
 			T_Left = T_Last[idx - 1];
-			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Last[idx] +
+			T_New[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 	}
 
 	else
 	{
-		Physicial_Parameters(T_New[idx], &pho, &Ce, &lamd);
-		a = (lamd) / (pho*Ce);
-		h = Boundary_Condition(i, ny, dy, ccml, H_Init);
 		if (j == 0 && i != 0 && i != ny - 1) //1
 		{
+			T_Middle = T_New[idx];
 			T_Up = T_New[idx + D];
-			T_Down = T_New[idx + D] - 2 * dx*h*(T_New[idx] - Tw) / lamd;
+			T_Down = T_New[idx + D] - 2 * dx * h* (T_Middle - Tw) / lamd;
 			T_Right = T_New[idx + 1];
 			T_Left = T_New[idx - 1];
-			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_New[idx] +
+			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (j == nx - 1 && i != 0 && i != ny - 1)  //2
 		{
+			T_Middle = T_New[idx];
 			T_Up = T_New[idx - D];
 			T_Down = T_New[idx - D];
 			T_Right = T_New[idx + 1];
 			T_Left = T_New[idx - 1];
-			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_New[idx] +
+			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (i == 0 && j != 0 && j != nx - 1)  //3
@@ -125,11 +131,12 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 		}
 		else if (i == ny - 1 && j != 0 && j != nx - 1)  //4
 		{
+			T_Middle = T_New[idx];
 			T_Up = T_New[idx + D];
 			T_Down = T_New[idx - D];
 			T_Right = T_New[idx - 1];
 			T_Left = T_New[idx - 1];
-			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_New[idx] +
+			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (i == 0 && j == 0)  //5
@@ -138,11 +145,12 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 		}
 		else if (j == 0 && i == ny - 1)  //6
 		{
+			T_Middle = T_New[idx];
 			T_Up = T_New[idx + D];
 			T_Down = T_New[idx + D];
 			T_Right = T_New[idx - 1];
 			T_Left = T_New[idx - 1];
-			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_New[idx] +
+			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 		else if (j == nx - 1 && i == 0)   //7
@@ -151,20 +159,22 @@ __global__ void addKernel(float *T_New, float *T_Last, float *ccml, float *H_Ini
 		}
 		else if (j == nx - 1 && i == ny - 1) //8
 		{
+			T_Middle = T_New[idx];
 			T_Up = T_New[idx - D];
 			T_Down = T_New[idx - D];
 			T_Right = T_New[idx - 1];
 			T_Left = T_New[idx - 1];
-			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_New[idx] +
+			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
-		else
+		else if (j != 0 && j != nx - 1 && i != 0 && i != ny - 1)  //9
 		{
+			T_Middle = T_New[idx];
 			T_Up = T_New[idx + D];
 			T_Down = T_New[idx - D];
 			T_Right = T_New[idx + 1];
 			T_Left = T_New[idx - 1];
-			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_New[idx] +
+			T_Last[idx] = a*(tao / (dx*dx))*T_Up + a*(tao / (dx*dx))*T_Down + (1 - 2 * a*(tao / (dx*dx)) - 2 * a*(tao / (dy*dy)) + tao*Vcast / dy)*T_Middle +
 				a*(tao / (dy*dy))*T_Right + (a*(tao / (dy*dy)) - tao*Vcast / dy)*T_Left;
 		}
 	}
@@ -174,7 +184,7 @@ int main()
 {
 	const int nx = 11, ny = 3000;   // nx is the number of grid in x direction, ny is the number of grid in y direction.
 	int num_blocks = 1, num_threadsx = 1, num_threadsy = 1; // block number(1D)  thread number in x and y dimension(2D)
-	int tnpts = 10000.0;  // time step
+	int tnpts = 10000;  // time step
 	float T_Cast = 1558.0, Lx = 0.125, Ly = 28.599, t_final = 2000.0, dx, dy, tao;  // T_Cast is the casting temperature Lx and Ly is the thick and length of steel billets
 	float *T_Init;
 	float ccml[Section + 1] = { 0.0,0.2,0.4,0.6,0.8,1.0925,2.27,4.29,5.831,9.6065,13.6090,19.87014,28.599 }; // The cooling sections
@@ -260,12 +270,12 @@ cudaError_t addWithCuda(float *T_Init, float dx, float dy, float tao, int nx, in
 	// Launch a kernel on the GPU with one thread for each element.
 	for (int i = 0; i < tnpts; i++)
 	{
-		addKernel << <numBlocks, threadsPerBlock >> >(dev_T_New, dev_T_Last, dev_ccml, dev_H_Init, dx, dy, tao, nx, ny, dstOut);
+		MainKernel << <numBlocks, threadsPerBlock >> >(dev_T_New, dev_T_Last, dev_ccml, dev_H_Init, dx, dy, tao, nx, ny, dstOut);
 		dstOut = !dstOut;
 
 		if (i % Num_Iter == 0) {
 			HANDLE_ERROR(cudaMemcpy(T_Result, dev_T_Last, nx * ny * sizeof(float), cudaMemcpyDeviceToHost));
-			printf("time_step = %d \n", i);
+			printf("time_step = %d  simulation time is %f\n", i, i*tao);
 			printf("%f, %f, %f", T_Result[0], T_Result[(nx - 1)*(ny - 1) - nx], T_Result[(nx - 1)*(ny - 1)]);
 			printf("\n");
 		}
